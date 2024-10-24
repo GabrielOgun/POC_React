f48a5ce11fa42c33db2328a5b615982b309de782
Gabriel <gahmarks@hotmail.com>
2024-10-24 13:30:37 -0300

    Updates

---
+++ _next/static/PvF9dNjyOtwJyrM8LkAXx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/382f72df72686a54.css","static/chunks/pages/index-2e61be334a40b642.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
